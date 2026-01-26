1fb8a516722644aaac76a7dc84e0d7f44b2291d0
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-26 13:35:01 +0200

    Added Task 02

---
+++ Task 02/Code/01.dart
@@ -0,0 +1,10 @@
+void main() {
+  String myTeam = "Real Madrid";
+  int numberOfMember = 11;
+  int champions = 26;
+
+  print("my Team is $myTeam , has $numberOfMember players and has $champions");
+
+  // output??
+  // my Team is Real Madried , has 11 players and has 26
+}
